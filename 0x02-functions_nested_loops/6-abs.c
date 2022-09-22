bde0cf95a9145aa98179ace16eef7b56b225b91f
Jamova01 <jorgemova01@gmail.com>
2022-09-22 11:27:50 -0500

    C function that computes the absolute value of an number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+/**
+ * _abs - C function that computes the absolute value of an number
+ * @n: Number that will be checked
+ * Return: 0
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+n = (-1) * n;
+return (n);
+}
+else
+return (n);
+}
