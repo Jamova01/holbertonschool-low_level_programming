e9e3c682ca438085040736098ea7850cbabc530a
Jamova01 <jorgemova01@gmail.com>
2022-09-22 10:31:11 -0500

    function that checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+/**
+ * _islower - function that checks for lowercase character
+ * @c: c is the letter that will be checked
+ * Return: 0
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+return (0);
+}
