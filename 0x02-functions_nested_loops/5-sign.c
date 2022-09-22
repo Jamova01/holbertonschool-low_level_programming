5f00a4641f29e0c4da3d74ffe63ae6aa4433ff52
Jamova01 <jorgemova01@gmail.com>
2022-09-22 11:11:46 -0500

    C  function that prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,7 +7,7 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-_putchar(43);    
+_putchar(43);
 return (1);
 }
 else if (n == 0)
@@ -15,7 +15,7 @@ else if (n == 0)
 _putchar(48);
 return (0);
 }
-else if ( n < 0)
+else if (n < 0)
 {
 _putchar(45);
 return (-1);
